49fe23cb5b1bc6ab28465117a35b767211d789be
Esko Suomi <esko.suomi@solita.fi>
2024-10-15 11:39:20 +0300

    code and configuration cleanup

--- src/main/java/fi/digitraffic/ura/kooste/web/KoosteResource.java
+++ src/main/java/fi/digitraffic/ura/kooste/web/KoosteResource.java
@@ -1,17 +1,11 @@
 package fi.digitraffic.ura.kooste.web;
 
-import fi.digitraffic.ura.kooste.publications.model.Publication;
 import fi.digitraffic.ura.kooste.publications.PublicationsService;
-import io.quarkus.amazon.s3.runtime.S3Crt;
+import fi.digitraffic.ura.kooste.publications.model.Publication;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
-import software.amazon.awssdk.transfer.s3.S3TransferManager;
 
 import java.util.List;
 import java.util.Locale;
@@ -19,20 +13,6 @@ import java.util.Locale;
 
 @Path("/")
 public class KoosteResource {
-
-    @Inject
-    S3Client s3Client;
-
-    @Inject
-    @S3Crt
-    S3AsyncClient s3AsyncClient;
-
-    @Inject
-    @S3Crt
-    S3TransferManager transferManager;
-
-    @Inject
-    SecretsManagerClient secretsManagerClient;
 
     private final PublicationsService publicationsService;
 
